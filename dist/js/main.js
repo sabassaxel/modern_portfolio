738e7dea907778ebf9f31a28ff94c143bde39345
Axel <axel.sabass@yahoo.co.uk>
2020-02-28 21:21:39 +0000

    JS re-committed

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,15 +17,17 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
-    //Set Menu State
+
+    // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
-    menuNav.classList.aremove("show");
+    menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
-    //Set Menu State
+
+    // Set Menu State
     showMenu = false;
   }
 }
